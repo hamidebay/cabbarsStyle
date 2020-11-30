12813e57950326a37e360f93f8c0fae4568b0de4
Hamide Bay <hamide.bay@baycom.ch>
2020-11-30 20:54:31 +0100

    else if block Zeile 12+13 eingefügt

--- cabbarsStyle.js
+++ cabbarsStyle.js
@@ -1,12 +1,24 @@
-const marke = "vw";
+const marke = "bmw";
 const color = "black";
-const year = 2017;
+const year = 2020;
 const customer_old = 1;
 
-// cabbarsStyle 
+// cabbarsStyle
 
-if (customer_old > 5) { console.log("Araban tamir edilecek"); }
-else if ((marke == "bmw") && (color == "red") && ((year >= 2010) && (year <= 2019))) { console.log(marke + " tamir edilecek"); }
-else if ((marke == "audi") && ((color == "red") || (color == "black")) && (((year >= 2005) && (year <= 2010)) || ((year >= 2014) && (year <= 2020)))) { console.log(marke + " tamir edilecek"); }
-else if ((marke == "vw") && (color == "black") && ((year >= 2001) && (year <= 2018))) { console.log(marke + " tamir edilecek"); }
-else { console.log("üzgünüm sizi kabul edemem"); }
+if (customer_old > 5) {
+  console.log("Araban tamir edilecek");
+} else if (marke == "bmw" && color == "red" && year >= 2010 && year <= 2019) {
+  console.log(marke + " tamir edilecek");
+} else if (marke == "bmw" && color == "black" && year == 2020) {
+  console.log(marke + " tamir edilecek");
+} else if (
+  marke == "audi" &&
+  (color == "red" || color == "black") &&
+  ((year >= 2005 && year <= 2010) || (year >= 2014 && year <= 2020))
+) {
+  console.log(marke + " tamir edilecek");
+} else if (marke == "vw" && color == "black" && year >= 2001 && year <= 2018) {
+  console.log(marke + " tamir edilecek");
+} else {
+  console.log("üzgünüm sizi kabul edemem");
+}
