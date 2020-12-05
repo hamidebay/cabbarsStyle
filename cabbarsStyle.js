089e83cbe460f0bbd3cbcdab83079e99c7533de2
Hamide Bay <hamide.bay@baycom.ch>
2020-12-05 22:16:45 +0100

    const eingefügt zeile 5+6

--- cabbarsStyle.js
+++ cabbarsStyle.js
@@ -2,23 +2,25 @@ const marke = "bmw";
 const color = "black";
 const year = 2020;
 const customer_old = 1;
+const message_Negativ = "üzgünüm sizi kabul edemem";
+const message_Positive = "tamir edilecek";
 
 // cabbarsStyle
 
 if (customer_old > 5) {
-  console.log("Araban tamir edilecek");
+  console.log(message_Positive);
 } else if (marke == "bmw" && color == "red" && year >= 2010 && year <= 2019) {
-  console.log(marke + " tamir edilecek");
+  console.log(marke +"" + message_Positive);
 } else if (marke == "bmw" && color == "black" && year == 2020) {
-  console.log(marke + " tamir edilecek");
+  console.log(marke + "" +message_Positive);
 } else if (
   marke == "audi" &&
   (color == "red" || color == "black") &&
   ((year >= 2005 && year <= 2010) || (year >= 2014 && year <= 2020))
 ) {
-  console.log(marke + " tamir edilecek");
+  console.log(marke + ""+ message_Positive);
 } else if (marke == "vw" && color == "black" && year >= 2001 && year <= 2018) {
-  console.log(marke + " tamir edilecek");
+  console.log(marke + "" + message_Positive);
 } else {
-  console.log("üzgünüm sizi kabul edemem");
+  console.log(message_Negativ);
 }
